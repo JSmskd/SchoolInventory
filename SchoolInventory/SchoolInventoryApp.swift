4feb01b13ed795b744c8267afe62f9cd6eac66a9
Neha Darji <ndarji7094@stu.d214.org>
2024-12-05 10:57:03 -0600

    Initial Commit

---
+++ SchoolInventory/SchoolInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolInventoryApp.swift
+//  SchoolInventory
+//
+//  Created by Neha M. Darji on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SchoolInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
