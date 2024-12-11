42520d3e085bdcccf2fc3a04b1464677dc103f3b
Aneena Ginson <aginson7135@stu.d214.org>
2024-12-11 10:37:01 -0600

    added dawg pound logo

--- SchoolInventory/ContentView.swift
+++ SchoolInventory/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("DawgPound")
+                .resizable()
+                .frame(width: 300, height: 170)
+
         }
         .padding()
     }
