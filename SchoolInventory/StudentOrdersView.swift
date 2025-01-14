9e67a02183a50f5271c5ec2797c00e106064102b
Neha Darji <ndarji7094@stu.d214.org>
2025-01-14 11:07:25 -0600

    Made the list visible in the StudentOrderView

--- SchoolInventory/StudentOrdersView.swift
+++ SchoolInventory/StudentOrdersView.swift
@@ -14,9 +14,10 @@ struct StudentOrdersView: View {
             .font(.custom("Impact", size: 70))
             .foregroundColor(.darkOrange)
         
+        HeaderOrderView(orderItems: $orderItems)
+        List(orderItems, id: \.self) { currentOrderItem in
+            OrderListView(currentOrderItem: currentOrderItem)
+        }
+        
     }
-}
-
-#Preview {
-    StudentOrdersView()
 }
