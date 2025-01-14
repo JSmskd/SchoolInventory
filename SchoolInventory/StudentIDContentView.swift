e2435e7a3abddbea3d06b489ceaeed87976299c0
Haasini Police <hpolice5296@stu.d214.org>
2025-01-14 09:15:34 -0800

    i added code to each view

--- SchoolInventory/StudentIDContentView.swift
+++ SchoolInventory/StudentIDContentView.swift
@@ -5,3 +5,15 @@
 //  Created by Haasini Kala R. Police on 1/14/25.
 //
 
+import SwiftUI
+
+struct StudentIDContentView: View {
+    
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    StudentIDContentView()
+}
