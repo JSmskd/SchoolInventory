cdb15ec2906663295e61fd25f1083af668981ed4
Aneena Ginson <aginson7135@stu.d214.org>
2025-01-14 11:00:45 -0600

    searchbar

---
+++ SchoolInventory/SearchBarView.swift
@@ -0,0 +1,41 @@
+//
+//  SearchBarView.swift
+//  SchoolInventory
+//
+//  Created by Aneena M. Ginson on 1/14/25.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    private var listOfCountry = countryList
+    @State var searchText = ""
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(countries, id: \.self) { counry in
+                    HStack{
+                        Text(counry.capitalized)
+                        Spacer()
+                        Image(systemName: "figure.walk")
+                            .foregroundColor(Color.blue)
+                    }
+                    .padding()
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Countries")
+        }
+      
+    }
+    var countries: [String] {
+        let lcCountries = listOfCountry.map { $0.lowercased() }
+        return searchText == "" ? lcCountries : lcCountries.filter {
+            $0.contains(searchText.lowercased())
+        }
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
