9f2092415bd5947715c018a5a7672ffb06d1460f
Neha Darji <ndarji7094@stu.d214.org>
2025-02-06 10:21:54 -0600

    Created @State variables

--- SchoolInventory/InventoryView.swift
+++ SchoolInventory/InventoryView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct InventoryView: View {
+    @State var shirts = ""
+    @State var sweatpants = ""
     var body: some View {
         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
     }
