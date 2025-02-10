44d00c2b995141558f09dddab4d8af4faded3536
Haasini Police <hpolice5296@stu.d214.org>
2025-02-10 09:05:33 -0800

    i added navigation links and made buttons for each item

---
+++ SchoolInventory/CrenecksView.swift
@@ -0,0 +1,21 @@
+//
+//  CrenecksView.swift
+//  SchoolInventory
+//
+//  Created by Haasini Kala R. Police on 2/10/25.
+//
+
+import SwiftUI
+
+struct CrewnecksView: View {
+    var body: some View {
+        VStack {
+            Text("Grey Crewnecks")
+            Text("Orange Crewnecks")
+        }
+    }
+   
+}
+#Preview {
+    CrewnecksView()
+}
---
+++ SchoolInventory/HoodiesView.swift
@@ -0,0 +1,21 @@
+//
+//  HoodiesView.swift
+//  SchoolInventory
+//
+//  Created by Haasini Kala R. Police on 2/10/25.
+//
+
+import SwiftUI
+
+struct HoodiesView: View {
+    var body: some View {
+        VStack {
+            Text("Grey Hoodies")
+            Text("Orange Hoodies")
+        }
+    }
+   
+}
+#Preview {
+    HoodiesView()
+}
