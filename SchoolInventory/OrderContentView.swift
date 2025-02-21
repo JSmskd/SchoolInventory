3c3e8febd9688a2c9fdf85de124ec9433bde5612
Neha Darji <ndarji7094@stu.d214.org>
2025-02-21 10:28:03 -0600

    Deleted view and commented unneeded view out

--- SchoolInventory/OrderContentView.swift
+++ SchoolInventory/OrderContentView.swift
@@ -5,48 +5,48 @@
 //  Created by Neha M. Darji on 1/16/25.
 //
 
-import SwiftUI
-import SwiftData
-
-struct OrderContentView: View {
-    @Environment(\.modelContext) var context
-    @Query var barcodeNumbers: [Order]
-    @State var enteredBarcodeNumber = 0
-    @State var enteredSize = ""
-    @State var enteredClothingItem = ""
-   
-    
-    var body: some View {
-        VStack {
-            HStack {
-                TextField("Enter barcode Number", value: $enteredBarcodeNumber, format: .number)
-                TextField("Enter size", text: $enteredSize)
-                TextField("Enter item name", text: $enteredClothingItem)
-                
-                Button("+") {
-                    let orders = Order(barcodeNumber: enteredBarcodeNumber, size: enteredSize, clothingItem: enteredClothingItem)
-                    context.insert(orders)
-                    
-                    enteredBarcodeNumber = 0
-                    enteredSize = ""
-                    enteredClothingItem = ""
-                    
-                }
-            }
-            List {
-                ForEach(barcodeNumbers) { currentBarcodeNumber in
-                    VStack {
-                        Text(currentBarcodeNumber.size)
-                        Text(currentBarcodeNumber.clothingItem)
-                    }
-                }
-            }
-            
-        }
-        
-    }
-}
-
-#Preview {
-    OrderContentView()
-}
+//import SwiftUI
+//import SwiftData
+//
+//struct OrderContentView: View {
+//    @Environment(\.modelContext) var context
+//    @Query var barcodeNumbers: [Order]
+//    @State var enteredBarcodeNumber = 0
+//    @State var enteredSize = ""
+//    @State var enteredClothingItem = ""
+//   
+//    
+//    var body: some View {
+//        VStack {
+//            HStack {
+//                TextField("Enter barcode Number", value: $enteredBarcodeNumber, format: .number)
+//                TextField("Enter size", text: $enteredSize)
+//                TextField("Enter item name", text: $enteredClothingItem)
+//                
+//                Button("+") {
+//                    let orders = Order(barcodeNumber: enteredBarcodeNumber, size: enteredSize, clothingItem: enteredClothingItem)
+//                    context.insert(orders)
+//                    
+//                    enteredBarcodeNumber = 0
+//                    enteredSize = ""
+//                    enteredClothingItem = ""
+//                    
+//                }
+//            }
+//            List {
+//                ForEach(barcodeNumbers) { currentBarcodeNumber in
+//                    VStack {
+//                        Text(currentBarcodeNumber.size)
+//                        Text(currentBarcodeNumber.clothingItem)
+//                    }
+//                }
+//            }
+//            
+//        }
+//        
+//    }
+//}
+//
+//#Preview {
+//    OrderContentView()
+//}
