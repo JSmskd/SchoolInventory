89edfae83be4b10a45cc7b513f04efeeb8456742
Aneena Ginson <aginson7135@stu.d214.org>
2025-02-27 10:56:14 -0600

    added the images

--- SchoolInventory/ShirtView.swift
+++ SchoolInventory/ShirtView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 struct ShirtView: View {
     var body: some View {
         VStack {
-            Text("Grey Shirt")
-            Text("Orange Shirt")
+            HStack {
+                
+                Image("Gildan5000")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .border(.brown, width: 2)
+                Image("Bella3001CVC")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .border(.brown, width: 2)
+            }
+            HStack {
+                Text("Orange Shirt")
+                Text("White Shirt")
+            }
         }
     }
-   
 }
+
 #Preview {
     ShirtView()
 }
