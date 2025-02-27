ffa5cae1db00450598758527743c65c4e74a8765
Neha Darji <ndarji7094@stu.d214.org>
2025-02-27 11:04:06 -0600

    Changed width and color of border of images

--- SchoolInventory/ShirtView.swift
+++ SchoolInventory/ShirtView.swift
@@ -15,11 +15,11 @@ struct ShirtView: View {
                 Image("Gildan5000")
                     .resizable()
                     .frame(width: 200, height: 200)
-                    .border(.brown, width: 2)
+                    .border(.orange, width: 5)
                 Image("Bella3001CVC")
                     .resizable()
                     .frame(width: 200, height: 200)
-                    .border(.brown, width: 2)
+                    .border(.orange, width: 5)
             }
             HStack {
                 Text("Orange Shirt")
