57f4987a26cbab352d11a80d8e57c25b4a1db565
Haasini Police <hpolice5296@stu.d214.org>
2025-03-05 08:39:53 -0800

    i added an edit button into crewnecks view

--- SchoolInventory/CrenecksView.swift
+++ SchoolInventory/CrenecksView.swift
@@ -6,16 +6,50 @@
 //
 
 import SwiftUI
-
 struct CrewnecksView: View {
+    @State private var isEditing = false  
+    @State private var greyCrewnecks = "Grey Crewnecks"
+    @State private var orangeCrewnecks = "Orange Crewnecks"
+
     var body: some View {
-        VStack {
-            Text("Grey Crewnecks")
-            Text("Orange Crewnecks")
+        NavigationView {
+            VStack {
+                if isEditing {
+                    TextField("Grey Crewnecks", text: $greyCrewnecks)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+
+                    TextField("Orange Crewnecks", text: $orangeCrewnecks)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                } else {
+                    Text(greyCrewnecks)
+                    Text(orangeCrewnecks)
+                }
+            }
+            .navigationTitle("Crewnecks")
+            .navigationBarItems(
+                trailing: Button(isEditing ? "Done" : "Edit") {
+                    isEditing.toggle()
+                }
+            )
         }
     }
-   
 }
+
 #Preview {
     CrewnecksView()
 }
+
+//struct CrewnecksView: View {
+//    var body: some View {
+//        VStack {
+//            Text("Grey Crewnecks")
+//            Text("Orange Crewnecks")
+//        }
+//    }
+//   
+//}
+//#Preview {
+//    CrewnecksView()
+//}
