e08ec82a4e79dd19ab6afce5ca66834af40d0ae3
Neha Darji <ndarji7094@stu.d214.org>
2025-03-05 10:38:58 -0600

    Changed the name of shirts to the name provided by the client

--- SchoolInventory/ShirtView.swift
+++ SchoolInventory/ShirtView.swift
@@ -16,14 +16,18 @@ struct ShirtView: View {
                     .resizable()
                     .frame(width: 200, height: 200)
                     .border(.orange, width: 5)
+                    .offset(x: -5, y: -240)
                 Image("Bella3001CVC")
                     .resizable()
                     .frame(width: 200, height: 200)
                     .border(.orange, width: 5)
+                    .offset(x: 5, y: -240)
             }
             HStack {
                 Text("Gildan5000")
+                    .offset(x: -45, y: -235)
                 Text("Bella3001CVC")
+                    .offset(x: 45, y: -235)
             }
         }
     }
