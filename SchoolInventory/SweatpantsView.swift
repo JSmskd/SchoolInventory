027fe01fcf7c6b8a5c627d5837164f25687ffd54
Haasini Police <hpolice5296@stu.d214.org>
2025-03-05 08:32:43 -0800

    i added an edit button into sweatpants view

--- SchoolInventory/SweatpantsView.swift
+++ SchoolInventory/SweatpantsView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
-
 struct SweatpantsView: View {
+    @State private var isEditing = false
+    @State private var greySweats = "Grey Sweats"
+    @State private var orangeSweats = "Orange Sweats"
+
     var body: some View {
-        VStack {
-            Text("Grey Sweats")
-            Text("Orange Sweats")
+        NavigationView {
+            VStack {
+                if isEditing {
+                    TextField("Grey Sweats", text: $greySweats)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+
+                    TextField("Orange Sweats", text: $orangeSweats)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                } else {
+                    Text(greySweats)
+                    Text(orangeSweats)
+                }
+            }
+            .navigationTitle("Sweatpants")
+            .navigationBarItems(
+                trailing: Button(isEditing ? "Done" : "Edit") {
+                    isEditing.toggle()
+                }
+            )
         }
     }
-   
 }
+
 #Preview {
     SweatpantsView()
+
+//struct SweatpantsView: View {
+//    var body: some View {
+//        VStack {
+//            Text("Grey Sweats")
+//            Text("Orange Sweats")
+//        }
+//    }
+//   
+//}
+//#Preview {
+//    SweatpantsView()
 }
