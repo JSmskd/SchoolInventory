a812348810f82d2de07d548f18085ee4903db4b9
Aneena Ginson <aginson7135@stu.d214.org>
2025-03-17 10:28:51 -0500

    created total orders view

---
+++ SchoolInventory/TotalOrders.swift
@@ -0,0 +1,25 @@
+//
+//  TotalOrders.swift
+//  SchoolInventory
+//
+//  Created by Aneena M. Ginson on 3/17/25.
+//
+
+import SwiftUI
+
+struct TotalOrders: View {
+    var body: some View {
+        VStack {
+            Text("Total Orders")
+                .font(.largeTitle)
+                .padding()
+
+            Text("You have 0 orders")
+              
+        }
+    }
+}
+
+#Preview {
+    TotalOrders()
+}
