a46eb2563ff17f12ddaf792b4fb831aeccd10c83
Aneena Ginson <aginson7135@stu.d214.org>
2025-05-02 11:04:35 -0500

    added scrollview

--- SchoolInventory/InventoryView.swift
+++ SchoolInventory/InventoryView.swift
@@ -15,58 +15,61 @@ extension Color {
 
 struct InventoryView: View {
     var body: some View {
-        NavigationView {
-            VStack(spacing: 20) {
-                NavigationLink(destination: BlanksView()) {
-                    Text("Blanks")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.darkBrown)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+      
+            NavigationView {
+                ScrollView{
+                VStack(spacing: 20) {
+                    NavigationLink(destination: BlanksView()) {
+                        Text("Blanks")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.darkBrown)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    NavigationLink(destination: ShirtView()) {
+                        Text("Shirts")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.darkOrange)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    
+                    NavigationLink(destination: SweatpantsView()) {
+                        Text("Sweatpants")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.darkBrown)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    
+                    NavigationLink(destination: HoodiesView()) {
+                        Text("Hoodies")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.darkOrange)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    
+                    NavigationLink(destination: CrewnecksView()) {
+                        Text("Crewnecks")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.darkBrown)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
                 }
-                NavigationLink(destination: ShirtView()) {
-                    Text("Shirts")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.darkOrange)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
+                .padding()
+                .navigationTitle("Inventory")
                 
-                NavigationLink(destination: SweatpantsView()) {
-                    Text("Sweatpants")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.darkBrown)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                
-                NavigationLink(destination: HoodiesView()) {
-                    Text("Hoodies")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.darkOrange)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                
-                NavigationLink(destination: CrewnecksView()) {
-                    Text("Crewnecks")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.darkBrown)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
             }
-            .padding()
-            .navigationTitle("Inventory")
-         
         }
     }
-}
+    }
 
 #Preview {
     InventoryView()
