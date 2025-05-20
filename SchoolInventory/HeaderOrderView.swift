254470372e8520235faa374a54f11f14faf3d08d
Neha Darji <ndarji7094@stu.d214.org>
2025-05-20 10:59:44 -0500

    Commented out unused views in the app

--- SchoolInventory/HeaderOrderView.swift
+++ SchoolInventory/HeaderOrderView.swift
@@ -7,28 +7,28 @@
 
 import SwiftUI
 
-struct HeaderOrderView: View {
-    @State var newOrderItemName: String = ""
-    @State var newStudentID: Int?
-    @Binding var orderItems: [OrderItem]
-    @AppStorage("OrderCharacteristics") var orderCharacteristics: String = ""
-    var body: some View {
-        HStack{
-            TextField("ID", value: $newStudentID, format: .number)
-                .textFieldStyle(.roundedBorder)
-            TextField("Item name", text: $newOrderItemName)
-                .textFieldStyle(.roundedBorder)
-            Button("+") {
-                guard let number = newStudentID else {return}
-                let newOrderItem = OrderItem(nameOfItem: newOrderItemName, studentID: number)
-                orderItems.append(newOrderItem)
-                
-                newOrderItemName = ""
-                newStudentID = nil
-            }
-            .bold()
-            .font(.largeTitle)
-        }
-        .padding()
-    }
-}
+//struct HeaderOrderView: View {
+//    @State var newOrderItemName: String = ""
+//    @State var newStudentID: Int?
+//    @Binding var orderItems: [OrderItem]
+//    @AppStorage("OrderCharacteristics") var orderCharacteristics: String = ""
+//    var body: some View {
+//        HStack{
+//            TextField("ID", value: $newStudentID, format: .number)
+//                .textFieldStyle(.roundedBorder)
+//            TextField("Item name", text: $newOrderItemName)
+//                .textFieldStyle(.roundedBorder)
+//            Button("+") {
+//                guard let number = newStudentID else {return}
+//                let newOrderItem = OrderItem(nameOfItem: newOrderItemName, studentID: number)
+//                orderItems.append(newOrderItem)
+//                
+//                newOrderItemName = ""
+//                newStudentID = nil
+//            }
+//            .bold()
+//            .font(.largeTitle)
+//        }
+//        .padding()
+//    }
+//}
